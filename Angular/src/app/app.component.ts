e260eef4272eb64e1194a3471f879ed0748b3659
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-05-24 12:30:28 +0300

    chore(angular): ng update 18 → 19

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -8,9 +8,10 @@ import {
 import { EmployeesService } from './employees.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   employees: Employee[] = [];
