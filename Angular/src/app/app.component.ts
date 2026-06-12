3248a9df7bdc93749f8888bb8c4afa5b5e0b68ac
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-12 14:42:19 +0300

    Update Angular to v22

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import {
   Employee,
   DragChangeEvent,
@@ -14,6 +14,7 @@ import { DxButtonModule } from 'devextreme-angular/ui/button';
     selector: 'app-root',
     imports: [DxTreeListModule, DxButtonModule],
     templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.Eager,
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
